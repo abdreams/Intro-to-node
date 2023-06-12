55e8db79bacbaefa8f4ebb7b02efb9a06e7a02a7
abhay gupta <iabhay.gupta@gmail.com>
2023-06-12 13:14:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var superheroes =require("superheroes");
+var superVillains = require("supervillains");
+
+var mySuperVillain = superVillains.random();
+var mySuperHeroName = superheroes.random();
+console.log(mySuperHeroName);
+console.log(mySuperVillain);
